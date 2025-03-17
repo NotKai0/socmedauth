68355c98ce9466a8a809d98d1e51f2649b52790f
NotKai0 <kai.digiteer@gmail.com>
2025-03-17 14:56:52 +0800

    add posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_17_014518) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_064954) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "published_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
